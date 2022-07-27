fc6c91ea4fb9186c1e70e34ce955f1a2b2517b58
IsmailMutafchev <ismailmutafchev@gmail.com>
2022-07-27 20:43:01 +0100

    Update Footer
    Update the footer , add links to favicons

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -1,13 +1,28 @@
 import './Footer.css'
 
 export function Footer() {
-    return(<footer>
+    return (<footer>
         <ul>
             <li class="link" id="find_me">find me in:</li>
-            <li class="link" id="twitter"><a href=""><i class="fa-brands fa-twitter"></i></a></li>
-            <li class="link" id="facebook"><a href=""><i class="fa-brands fa-facebook-f"></i></a></li>
-            <li class="link" id="blank_bottom"></li>
-            <li class="link" id="github"><a href="https://github.com/ismailmutafchev">@ismailmutafchev<i class="fa-brands fa-github"></i></a></li>
+
+            <li class="link" id="twitter">
+                <a href="https://twitter.com/IMutafchev" target="_blank">
+                    <i class="fa-brands fa-twitter"></i>
+                </a>
+            </li>
+            <li class="link" id="facebook">
+                <a href="https://www.facebook.com/Smiley.Shady" target="_blank">
+                    <i class="fa-brands fa-facebook-f"></i>
+                </a>
+            </li>
+
+            <li class="link" id="blank_bottom">
+            </li>
+            <li class="link" id="github">
+                <a href="https://github.com/ismailmutafchev" target="_blank">@ismailmutafchev
+                    <i class="fa-brands fa-github"></i>
+                </a>
+            </li>
         </ul>
     </footer>)
 }
