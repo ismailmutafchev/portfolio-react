9e562260cd7a647357241b58b539a82f17c52bf2
IsmailMutafchev <ismailmutafchev@gmail.com>
2022-07-27 23:14:29 +0100

    add contact
    add contacts and hobies

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,36 @@ import './Submarine.scss'
 function About() {
     return (
         <section className="about">
+
+            <div className="details-interests">
+                <div className="about-me-headers">
+                    <h1>_about.me</h1>
+                    <p><span>Jack is a simple</span>, creative and responsive bootstrap 4 template for showcase your portfolio and personal information. It is designed and developed using all modern technologies (includes HTML5, CSS3) and bootstrap. If you are a designer, developer, freelancer or any other professional, this template is perfect fit for you. </p>
+                </div>
+                <div className="about-me-details">
+                    <div className="details-list">
+                        <div> 
+
+                        <h1>Personal Details</h1>
+                        <ul className="details-types">
+                            <li><p className='details-type'>phone number:</p> <p>fdsfsdfsd</p></li>
+                            <li><p className='details-type'>email:</p> <p>fdsfsdfsd</p></li>
+                            <li><p className='details-type'>whatsapp:</p> <p>fdsfsdfsd</p></li>
+                        </ul>
+                        </div>
+                    </div>
+                    <div className="about-me-intrests">
+                        <h1>My interests</h1>
+                        <ul className="details-types">
+                            <li><i class="fa-solid fa-futbol"></i></li>
+                            <li><i class="fa-solid fa-photo-film-music"></i></li>
+                            <li><i class="fa-solid fa-code"></i></li>
+                            <li><i class="fa-solid fa-popcorn"></i></li>
+                            <li><i class="fa-solid fa-gamepad-modern"></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
             <div className="sea">
                 <div className="circle-wrapper">
                     <div className="bubble"></div>
@@ -21,34 +51,6 @@ function About() {
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className="details-interests">
-                <div className="about-me-headers">
-                    <h1>_about.me</h1>
-                    <p><span>Jack is a simple</span>, creative and responsive bootstrap 4 template for showcase your portfolio and personal information. It is designed and developed using all modern technologies (includes HTML5, CSS3) and bootstrap. If you are a designer, developer, freelancer or any other professional, this template is perfect fit for you. </p>
-                </div>
-                <div className="about-me-details">
-                    <h1>Personal Details</h1>
-                    <div className="details-list">
-                        <ul className="details-type">
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                        </ul>
-                        <ul className="details-info">
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="about-me-intrests">
-                    <h1>My interests</h1>
                 </div>
             </div>
         </section>
