9fa222094b099048ef67758ddcecb148aed022be
IsmailMutafchev <ismailmutafchev@gmail.com>
2022-08-23 20:47:55 +0100

    Create Contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,38 @@
+import './Contact.scss'
+
 function Contact() {
     return (
-        <section>
-            <h1>Contact</h1>
+        <section className="contact-section">
+            <div className="contact-form">
+                <form action="/action_page.php">
+                    <div>
+                        <label htmlFor="fname">First Name</label>
+                        <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+                    </div>
+
+                    <div>
+                        <label htmlFor="lname">Last Name</label>
+                        <input
+                            type="text"
+                            id="lname"
+                            name="lastname"
+                            placeholder="Your last name.."
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="subject">Subject</label>
+                        <textarea
+                            id="subject"
+                            name="subject"
+                            placeholder="Write something.."
+                            style={{ height: 200 }}
+                            defaultValue={""}
+                        />
+                    </div>
+                    <input className='form-submit' type="submit" defaultValue="Submit" />
+                </form>
+            </div>
         </section>
     )
 }
